1a6562319acdc1b9fd2626545ec01c61d8146095
Jeremy <walfare@live.fr>
2019-10-16 16:42:03 +0200

    Classe voiture

---
+++ src/com/company/Voiture.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Voiture {
+    int nbpneu;
+    String modele;
+    int nbChevaux;
+
+    public Voiture(int nbpneu, String modele, int nbChevaux) {
+        this.nbpneu = nbpneu;
+        this.modele = modele;
+        this.nbChevaux = nbChevaux;
+    }
+
+    public int getNbpneu() {
+        return nbpneu;
+    }
+
+    public void setNbpneu(int nbpneu) {
+        this.nbpneu = nbpneu;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public int getNbChevaux() {
+        return nbChevaux;
+    }
+
+    public void setNbChevaux(int nbChevaux) {
+        this.nbChevaux = nbChevaux;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
